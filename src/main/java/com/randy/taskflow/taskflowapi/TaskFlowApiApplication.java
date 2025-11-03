f7982e5ec90d60b48528d22bf2beb248f526a4cb
randybrdn-401 <randybourdonpro@gmail.com>
2025-11-03 15:36:51 +0100

    Initial Spring Boot project

---
+++ src/main/java/com/randy/taskflow/taskflowapi/TaskFlowApiApplication.java
@@ -0,0 +1,13 @@
+package com.randy.taskflow.taskflowapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskFlowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskFlowApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/randy/taskflow/taskflowapi/TaskFlowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.randy.taskflow.taskflowapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskFlowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
